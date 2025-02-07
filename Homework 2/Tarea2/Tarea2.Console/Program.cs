2528e1e41b72a3ef687d07d4e77247b375d836c8
NatamaelMH <natanaelmarte5957@gmail.com>
2025-02-06 22:09:31 -0400

    Developing a program.
    In this program, I learned how to request a number from the user and read their input from the console.

---
+++ Homework 2/Tarea2/Tarea2.Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Natanael Marte 20241711
+        // Programa que determina si un número es par o impar
+
+        Console.Write("Ingrese un número: ");
+        int numero = Convert.ToInt32(Console.ReadLine()); 
+
+        
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine("El número " + numero + " es PAR.");
+        }
+        else
+        {
+            Console.WriteLine("El número " + numero + " es IMPAR.");
+        }
+
+        Console.ReadLine(); 
+    }
+}
+
