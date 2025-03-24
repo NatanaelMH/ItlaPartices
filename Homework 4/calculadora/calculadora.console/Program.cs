85660b53aaed87af65cc7a234cf4f50ebf589ece
NatamaelMH <natanaelmarte5957@gmail.com>
2025-03-24 09:24:08 -0400

    tarea complementaria
    Aqui pude hacer este codigo de un registro de pacientes , en la cual el usuario puede elegir
     agregar un paciente e ingresa sus datos.
    Puede buscar pacientes por ID.
    
    Puede mostrar todos los pacientes registrados.
     Puede salir del sistema.

--- Homework 4/calculadora/calculadora.console/Program.cs
+++ Homework 4/calculadora/calculadora.console/Program.cs
@@ -1,62 +1,90 @@
 ﻿using System;
-//Natanael Marte Hidalgo  20241711
-class Fraccion
-{
-    private int numerador;
-    private int denominador;
+using System.Collections.Generic;
 
-    public Fraccion()
+//Natanael Marte Hidalgo 20241711
+class Contacto
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string Telefono { get; set; }
+    public string Email { get; set; }
+    public string Direccion { get; set; }
+
+    public Contacto(int id, string nombre, string telefono, string email, string direccion)
     {
-        numerador = 0;
-        denominador = 1;
+        Id = id;
+        Nombre = nombre;
+        Telefono = telefono;
+        Email = email;
+        Direccion = direccion;
     }
 
-    public Fraccion(int numerador, int denominador)
+    public override string ToString()
     {
-        if (denominador != 0)
+        return $"{Id}    {Nombre}      {Telefono}      {Email}     {Direccion}";
+    }
+}
+
+class Agenda
+{
+    private Dictionary<int, Contacto> contactos = new Dictionary<int, Contacto>();
+
+    public void AgregarContacto(string nombre, string telefono, string email, string direccion)
+    {
+        int id = contactos.Count + 1;
+        Contacto nuevoContacto = new Contacto(id, nombre, telefono, email, direccion);
+        contactos.Add(id, nuevoContacto);
+    }
+
+    public void VerContactos()
+    {
+        Console.WriteLine("Id           Nombre          Telefono            Email           Dirección");
+        Console.WriteLine("___________________________________________________________________________");
+
+        foreach (var contacto in contactos.Values)
         {
-            this.numerador = numerador;
-            this.denominador = denominador;
+            Console.WriteLine(contacto);
+        }
+    }
+
+    public void EditarContacto(int id, string nombre, string telefono, string email, string direccion)
+    {
+        if (contactos.ContainsKey(id))
+        {
+            contactos[id].Nombre = nombre;
+            contactos[id].Telefono = telefono;
+            contactos[id].Email = email;
+            contactos[id].Direccion = direccion;
         }
         else
         {
-            Console.WriteLine("El denominador no puede ser cero.");
-            this.numerador = 0;
-            this.denominador = 1;
+            Console.WriteLine("Contacto no encontrado.");
         }
     }
 
-    public Fraccion Sumar(Fraccion otraFraccion)
+    public void EliminarContacto(int id)
     {
-        int nuevoNumerador = this.numerador * otraFraccion.denominador + otraFraccion.numerador * this.denominador;
-        int nuevoDenominador = this.denominador * otraFraccion.denominador;
-        return new Fraccion(nuevoNumerador, nuevoDenominador);
+        if (contactos.ContainsKey(id))
+        {
+            contactos.Remove(id);
+        }
+        else
+        {
+            Console.WriteLine("Contacto no encontrado.");
+        }
     }
 
-    public Fraccion Restar(Fraccion otraFraccion)
+    public Contacto BuscarContacto(int id)
     {
-        int nuevoNumerador = this.numerador * otraFraccion.denominador - otraFraccion.numerador * this.denominador;
-        int nuevoDenominador = this.denominador * otraFraccion.denominador;
-        return new Fraccion(nuevoNumerador, nuevoDenominador);
-    }
-
-    public Fraccion Multiplicar(Fraccion otraFraccion)
-    {
-        int nuevoNumerador = this.numerador * otraFraccion.numerador;
-        int nuevoDenominador = this.denominador * otraFraccion.denominador;
-        return new Fraccion(nuevoNumerador, nuevoDenominador);
-    }
-
-    public Fraccion Dividir(Fraccion otraFraccion)
-    {
-        int nuevoNumerador = this.numerador * otraFraccion.denominador;
-        int nuevoDenominador = this.denominador * otraFraccion.numerador;
-        return new Fraccion(nuevoNumerador, nuevoDenominador);
-    }
-
-    public void MostrarFraccion()
-    {
-        Console.WriteLine($"{numerador}/{denominador}");
+        if (contactos.ContainsKey(id))
+        {
+            return contactos[id];
+        }
+        else
+        {
+            Console.WriteLine("Contacto no encontrado.");
+            return null;
+        }
     }
 }
 
@@ -64,24 +92,112 @@ class Program
 {
     static void Main()
     {
-        Fraccion fraccion1 = new Fraccion(1, 2);
-        Fraccion fraccion2 = new Fraccion(3, 4);
+        Agenda agenda = new Agenda();
+        bool running = true;
 
-        Fraccion resultadoSuma = fraccion1.Sumar(fraccion2);
-        Console.Write("Resultado de la suma: ");
-        resultadoSuma.MostrarFraccion();
+        while (running)
+        {
+            Console.WriteLine("Mi Agenda Perrón");
+            Console.WriteLine("Bienvenido a tu lista de contactes");
+            Console.Write("1. Agregar Contacto      ");
+            Console.Write("2. Ver Contactos     ");
+            Console.Write("3. Buscar Contactos      ");
+            Console.Write("4. Modificar Contacto        ");
+            Console.Write("5. Eliminar Contacto     ");
+            Console.WriteLine("6. Salir");
+            Console.Write("Elige una opción: ");
 
-        Fraccion resultadoResta = fraccion1.Restar(fraccion2);
-        Console.Write("Resultado de la resta: ");
-        resultadoResta.MostrarFraccion();
+            int choice = Convert.ToInt32(Console.ReadLine());
 
-        Fraccion resultadoMultiplicacion = fraccion1.Multiplicar(fraccion2);
-        Console.Write("Resultado de la multiplicación: ");
-        resultadoMultiplicacion.MostrarFraccion();
+            switch (choice)
+            {
+                case 1:
+                    AddContact(agenda);
+                    break;
+                case 2:
+                    agenda.VerContactos();
+                    break;
+                case 4:
+                    EditContact(agenda);
+                    break;
+                case 5:
+                    DeleteContact(agenda);
+                    break;
+                case 3:
+                    SearchContact(agenda);
+                    break;
+                case 6:
+                    running = false;
+                    break;
+                default:
+                    Console.WriteLine("Opción no válida");
+                    break;
+            }
+        }
+    }
 
-        Fraccion resultadoDivision = fraccion1.Dividir(fraccion2);
-        Console.Write("Resultado de la división: ");
-        resultadoDivision.MostrarFraccion();
+    static void AddContact(Agenda agenda)
+    {
+        Console.WriteLine("Vamos a agregar ese contacte que te trae loco.");
+
+        Console.Write("Digite el Nombre: ");
+        var name = Console.ReadLine();
+        Console.Write("Digite el Teléfono: ");
+        var telefono = Console.ReadLine();
+        Console.Write("Digite el Email: ");
+        var email = Console.ReadLine();
+        Console.Write("Digite la dirección: ");
+        var address = Console.ReadLine();
+
+        agenda.AgregarContacto(name, telefono, email, address);
+    }
+
+    static void EditContact(Agenda agenda)
+    {
+        agenda.VerContactos();
+        Console.WriteLine("Digite un  Id de Contacto Para Editar");
+        int idSeleccionado = Convert.ToInt32(Console.ReadLine());
+
+        var contacto = agenda.BuscarContacto(idSeleccionado);
+        if (contacto != null)
+        {
+            Console.Write($"El nombre es: {contacto.Nombre}, Digite el Nuevo Nombre: ");
+            var name = Console.ReadLine();
+            Console.Write($"El Teléfono es: {contacto.Telefono}, Digite el Nuevo Teléfono: ");
+            var telefono = Console.ReadLine();
+            Console.Write($"El Email es: {contacto.Email}, Digite el Nuevo Email: ");
+            var email = Console.ReadLine();
+            Console.Write($"La dirección es: {contacto.Direccion}, Digite la nueva dirección: ");
+            var address = Console.ReadLine();
+
+            agenda.EditarContacto(idSeleccionado, name, telefono, email, address);
+        }
+    }
+
+    static void DeleteContact(Agenda agenda)
+    {
+        agenda.VerContactos();
+        Console.WriteLine("Digite un Id de Contacto Para Eliminar");
+        int idSeleccionado = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Seguro que desea eliminar? 1. Si, 2. No");
+        int opcion = Convert.ToInt32(Console.ReadLine());
+
+        if (opcion == 1)
+        {
+            agenda.EliminarContacto(idSeleccionado);
+        }
+    }
+
+    static void SearchContact(Agenda agenda)
+    {
+        Console.WriteLine("Digite un Id de Contacto Para Mostrar");
+        int idSeleccionado = Convert.ToInt32(Console.ReadLine());
+
+        var contacto = agenda.BuscarContacto(idSeleccionado);
+        if (contacto != null)
+        {
+            Console.WriteLine(contacto);
+        }
     }
 }
-
---
+++ Homework 4/tareaComplementaria/tareaComplementaria.console/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Natanael MArte Hidalgo 20241711
+class Paciente
+{
+    public int ID { get; set; }
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+    public string Diagnostico { get; set; }
+
+    public Paciente(int id, string nombre, int edad, string diagnostico)
+    {
+        ID = id;
+        Nombre = nombre;
+        Edad = edad;
+        Diagnostico = diagnostico;
+    }
+
+    public void MostrarInformacion()
+    {
+        Console.WriteLine($"ID: {ID} | Nombre: {Nombre} | Edad: {Edad} | Diagnóstico: {Diagnostico}");
+    }
+}
+
+
+class AdministradorPacientes
+{
+    private List<Paciente> listaPacientes = new List<Paciente>();
+    private int contadorID = 1;
+
+    public void AgregarPaciente(string nombre, int edad, string diagnostico)
+    {
+        Paciente nuevo = new Paciente(contadorID, nombre, edad, diagnostico);
+        listaPacientes.Add(nuevo);
+        contadorID++;
+        Console.WriteLine("Paciente agregado correctamente.\n");
+    }
+
+    public void MostrarPacientes()
+    {
+        if (listaPacientes.Count == 0)
+        {
+            Console.WriteLine("No hay pacientes registrados.\n");
+            return;
+        }
+
+        Console.WriteLine("Lista de Pacientes:");
+        foreach (var paciente in listaPacientes)
+        {
+            paciente.MostrarInformacion();
+        }
+        Console.WriteLine();
+    }
+
+    public void BuscarPaciente(int id)
+    {
+        var paciente = listaPacientes.Find(p => p.ID == id);
+        if (paciente != null)
+        {
+            Console.WriteLine("Paciente encontrado:");
+            paciente.MostrarInformacion();
+        }
+        else
+        {
+            Console.WriteLine("Paciente no encontrado.\n");
+        }
+    }
+
+    public void EliminarPaciente(int id)
+    {
+        var paciente = listaPacientes.Find(p => p.ID == id);
+        if (paciente != null)
+        {
+            listaPacientes.Remove(paciente);
+            Console.WriteLine("Paciente eliminado correctamente.\n");
+        }
+        else
+        {
+            Console.WriteLine("Paciente no encontrado.\n");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        AdministradorPacientes admin = new AdministradorPacientes();
+        int opcion;
+        do
+        {
+            Console.WriteLine("\nSistema de Registro de Pacientes");
+            Console.WriteLine("1. Agregar Paciente");
+            Console.WriteLine("2. Mostrar Pacientes");
+            Console.WriteLine("3. Buscar Paciente");
+            Console.WriteLine("4. Eliminar Paciente");
+            Console.WriteLine("5. Salir");
+            Console.Write("Seleccione una opción: ");
+            opcion = int.Parse(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    Console.Write("Nombre: ");
+                    string nombre = Console.ReadLine();
+                    Console.Write("Edad: ");
+                    int edad = int.Parse(Console.ReadLine());
+                    Console.Write("Diagnóstico: ");
+                    string diagnostico = Console.ReadLine();
+                    admin.AgregarPaciente(nombre, edad, diagnostico);
+                    break;
+                case 2:
+                    admin.MostrarPacientes();
+                    break;
+                case 3:
+                    Console.Write("Ingrese el ID del paciente: ");
+                    int idBuscar = int.Parse(Console.ReadLine());
+                    admin.BuscarPaciente(idBuscar);
+                    break;
+                case 4:
+                    Console.Write("Ingrese el ID del paciente a eliminar: ");
+                    int idEliminar = int.Parse(Console.ReadLine());
+                    admin.EliminarPaciente(idEliminar);
+                    break;
+                case 5:
+                    Console.WriteLine("Saliendo del sistema...");
+                    break;
+                default:
+                    Console.WriteLine("Opción no válida, intente nuevamente.");
+                    break;
+            }
+        } while (opcion != 5);
+    }
+}
+
