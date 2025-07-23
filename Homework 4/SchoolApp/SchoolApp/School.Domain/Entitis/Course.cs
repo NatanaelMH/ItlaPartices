70b5b768300186b366381aeec861b0078f148d46
NatamaelMH <natanaelmarte5957@gmail.com>
2025-07-23 17:44:09 -0400

    Tarea 3

---
+++ Homework 4/SchoolApp/SchoolApp/School.Domain/Entitis/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School.Domain.Entitis
+{
+    public class Course
+    {
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        public int DepartmentId { get; set; }
+    }
+}
---
+++ Homework 4/SchoolApp/SchoolApp/School.Infrastructure/Context/SchoolContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using School.Domain.Entities;
+
+
+
+namespace School.Infrastructure.Context
+{
+    public class SchoolContext
+    {
+        public SchoolContext() : base("name=SchoolConnection") { }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
