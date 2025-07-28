23a2290c9018b8dc9216083537a4bd1baf0f84d4
NatamaelMH <natanaelmarte5957@gmail.com>
2025-07-28 12:52:59 -0400

    Tarea 5 complementaria

---
+++ Crud/CrudSolution/CrudApi/Controllers/ProductController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CrudApi.Models;
+using CrudApi.Services;
+
+namespace CrudApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _service;
+
+        public ProductController(IProductService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll() => Ok(_service.GetAll());
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var product = _service.GetById(id);
+            if (product == null) return NotFound();
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            _service.Add(product);
+            return CreatedAtAction(nameof(Get), new { id = product.Id }, product);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Product product)
+        {
+            if (id != product.Id) return BadRequest();
+            _service.Update(product);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _service.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ Crud/CrudSolution/CrudApi/Services/ProductService.cs
@@ -0,0 +1,31 @@
+﻿using CrudApi.Models;
+
+namespace CrudApi.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly List<Product> _products = new();
+
+        public List<Product> GetAll() => _products;
+
+        public Product? GetById(int id) => _products.FirstOrDefault(p => p.Id == id);
+
+        public void Add(Product product)
+        {
+            product.Id = _products.Count + 1;
+            _products.Add(product);
+        }
+
+        public void Update(Product product)
+        {
+            var index = _products.FindIndex(p => p.Id == product.Id);
+            if (index != -1) _products[index] = product;
+        }
+
+        public void Delete(int id)
+        {
+            var product = _products.FirstOrDefault(p => p.Id == id);
+            if (product != null) _products.Remove(product);
+        }
+    }
+}
